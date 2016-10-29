5a20dc0d614f3155f8a597be3cf80608fad1dc0f
Jonas <jonabrau@mail.uni-mannheim.de>
2016-10-29 18:05:21 +0200

    Exercises update

--- src/providers/mock/training-data.ts
+++ src/providers/mock/training-data.ts
@@ -10,27 +10,41 @@
 
 
  var category_one: Exercise[] = [
-   {id:1, name:'Backstretching 1', description:'Move your Back', duration: 60, goal:'Helps back', image: 'assets/img/ruecken.jpg'},
-   {id:2, name:'Shoulderstretching 1', description:'Move your Shoulders', duration: 60, goal:'Helps back', image: 'assets/img/ruecken.jpg'},
-   {id:3, name:'Neckstretching 1', description:'Move your Neck', duration: 60, goal:'Helps back', image: 'assets/img/ruecken.jpg'},
-   {id:4, name:'Armstretching 1', description:'Move your Arms', duration: 60, goal:'Helps back', image: 'assets/img/ruecken.jpg'},
-   {id:5, name:'Fingerstretching 1', description:'Move your Fingers', duration: 60, goal:'Helps back', image: 'assets/img/ruecken.jpg'},
+   {id:1, name:'Press against Resistance', description:'Put both hands behind your head to create a resistance. Press your head with small pressure against your hands.\
+    Try not to move the hands or your head. Hold this tension for 10 seconds,\
+    then make a 10 second break and repeat this 3 times.', duration: 60, goal:'Strengthening of neck muscles', image: 'assets/img/training/neck1.PNG'},
+   {id:2, name:'Create Tension in your Neck', description:'Relax your shoulders and lean your head to one side. \
+   The hand on the opposite side increases the tension by being \
+   pressed in direction of the ground. Repeat and switch the side.', duration: 60, goal:'Relaxation of the neck muscles', image: 'assets/img/training/neck2.PNG'},
+   {id:3, name:'Circle your Head', description:'Lean your head forward and slowly roll from side to side.', duration: 60, goal:'Mobilisation of the cervical spine', image: 'assets/img/training/neck3.PNG'},
+   {id:4, name:'Incline your Head', description:'Lean your head to the left side and go back to the starting position.\
+    Then switch the side.', duration: 60, goal:'Relaxation of the neck muscles', image: 'assets/img/training/neck4.PNG'},
 
  ];
  var category_two: Exercise[] = [
-   {id:1, name:'Backstretching 2', description:'Move your Back', duration: 60, goal:'Helps back', image: 'assets/img/lende.jpg'},
-   {id:2, name:'Shoulderstretching 2', description:'Move your Shoulders', duration: 60, goal:'Helps back', image: 'assets/img/lende.jpg'},
-   {id:3, name:'Neckstretching 2', description:'Move your Neck', duration: 60, goal:'Helps back', image: 'assets/img/lende.jpg'},
-   {id:4, name:'Armstretching 2', description:'Move your Arms', duration: 60, goal:'Helps back', image: 'assets/img/lende.jpg'},
-   {id:5, name:'Fingerstretching 2', description:'Move your Fingers', duration: 60, goal:'Helps back', image: 'assets/img/lende.jpg'},
+   {id:1, name:'Circle your Shoulders', description:'Start with circling your shoulders backwards in small movements.\
+    Then try to maximise the size of circles.  After 30 seconds, switch the direction\
+     of the circulation and repeat the movement.', duration: 60, goal:'Mobilisation of the shoulder', image: 'assets/img/training/shoulder1.PNG'},
+   {id:2, name:'Raise your Shoulders', description:'Raise both shoulders at once up toward the ears. \
+   Hold, release and drop them. Afterwards repeat the movement.', duration: 60, goal:'Relaxation of the shoulder muscles', image: 'assets/img/training/shoulder2.PNG'},
+   {id:3, name:'Stretch and Strengthen your Shoulders', description:'Create a U-form with your arms, the elbows are at the height of your shoulders. \
+   Press your arms to the back. Hold this position some seconds. Afterwards put the hands together and pull the arms as much as possible in height of the shoulders to the front.\
+    Hold this position some seconds and repeat the movement.', duration: 60, goal:'Stretch and Strengthening of the shoulder muscles', image: 'assets/img/training/shoulder3.PNG'},
+   {id:4, name:'Keep your Shoulders together', description:'Clap your fingers at chest height. Create a tension by pressing the shoulder blades together and pull open the elbows.\
+    Hold for 10 seconds and release. Repeat the movement three times.', duration: 60, goal:'Strengthening of the shoulder muscles', image: 'assets/img/training/shoulder4.PNG'},
+
 
  ];
  var category_three: Exercise[] = [
-   {id:1, name:'Backstretching 3', description:'Move your Back', duration: 60, goal:'Helps back', image: 'assets/img/nacken.jpg'},
-   {id:2, name:'Shoulderstretching 3', description:'Move your Shoulders', duration: 60, goal:'Helps back', image: 'assets/img/nacken.jpg'},
-   {id:3, name:'Neckstretching 3', description:'Move your Neck', duration: 60, goal:'Helps back', image: 'assets/img/nacken.jpg'},
-   {id:4, name:'Armstretching 3', description:'Move your Arms', duration: 60, goal:'Helps back', image: 'assets/img/nacken.jpg'},
-   {id:5, name:'Fingerstretching 3', description:'Move your Fingers', duration: 60, goal:'Helps back', image: 'assets/img/nacken.jpge'},
+   {id:1, name:'Zoom with your Eyes', description:'Stretch your arm, the thumb points upwards. As long as you clearly see the thumb, move it slowly in direction of your face. \
+   Afterwards move it slowly back to the starting position. Repeat this movement.', duration: 60, goal:'Relaxation of the eye muscles', image: 'assets/img/training/eye1.PNG'},
+   {id:2, name:'Let your Eyes wander', description:'Select several items with different distances in your environment. Focus your view on each of this items a few seconds and then switch to the next one. \
+   At the last time, go the same item order back.', duration: 60, goal:'Flexibility of the eye lense', image: 'assets/img/training/eye2.PNG'},
+   {id:3, name:'Move your Eye ', description:'Slowly look with your open eyes to the left and then to the right side. Repeat it 5 times. Afterwards close your eyes for some seconds.\
+    Finally look for 5 times up and down in a slow movement. ', duration: 60, goal:'Strengthen of eye muscles', image: 'assets/img/training/eye3.PNG'},
+   {id:4, name:'Coordinate your Eyes and Hands', description:'Stretch your arms at the height of your shoulders. Turn your head to the left side. The left thumb points up whereas the right one points down.\
+    When turning the head to the other side, \
+    also switch the thumbs position. Repeat it 7 times.', duration: 60, goal:'Coordination of eyes and hands', image: 'assets/img/training/eye4.PNG'},
 
  ];
 
