98caca322ca59ae91fdc9afcf0bb081f7b813e47
Jonas <jonabrau@mail.uni-mannheim.de>
2016-11-01 21:23:44 +0100

    new color proposal and profile page in the same structure as knowledge detail page

--- src/pages/profile-page/profile-page.ts
+++ src/pages/profile-page/profile-page.ts
@@ -37,8 +37,8 @@ export class ProfilePage {
   };
   public lineChartColors: Array<any> = [
     { // grey
-      backgroundColor: 'rgba(39,174,96,0.2)',
-      borderColor: 'rgba(39,174,96,1)',
+      backgroundColor: 'rgba(26,188,156,0.2)',
+      borderColor: 'rgba(26,188,156,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
